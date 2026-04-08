eb6aa4d4836e05c07602d4ec4949d6a8fe782dff
Adriano Ruseler <adrianoruseler@gmail.com>
2026-04-08 13:35:16 -0300

    Update frontpage

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,4 +1,4 @@
-import type {ReactNode} from 'react';
+import type { ReactNode } from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
@@ -11,38 +11,42 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Administrator",
+    Svg: require("@site/static/img/build-design-game-svgrepo-com.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        This role is vital for organizations, schools, and universities that use
+        Moodle for their e-learning and training needs, as the administrator
+        ensures the platform runs smoothly and effectively.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Developer",
+    Svg: require("@site/static/img/development-web-development-svgrepo-com.svg")
+      .default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Due to Moodle's open-source nature, developers often engage with the
+        Moodle community through forums, contributing to projects, and staying
+        up-to-date with the latest developments.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Educator",
+    Svg: require("@site/static/img/blackboard-svgrepo-com.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        It goes beyond simply knowing how to use Moodle's features. Teaching
+        practices use Moodle's tools to create effective and engaging learning
+        experiences.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
