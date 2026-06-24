eb22342957bf851c458ff0ecf65c6a5812811b03
Adriano Ruseler <ruseler@utfpr.edu.br>
2026-06-24 11:55:56 -0300

    Update navbar.ts
    Add dropdown

--- navbar.ts
+++ navbar.ts
@@ -10,26 +10,74 @@ const navbarItems: NavbarItem[] = [
     {
         to: "/adm/intro", // Link to a page in your Dev docs
         label: "Administrator",
+        type: "dropdown",
         position: "left",
         activeBaseRegex: `/adm/`, // Highlight when any Dev doc is active
+        items: [
+            {
+                to: "/adm/category/docker", // Link to a page in your adm docs
+                label: "Docker",
+            },
+            {
+                to: "/adm/category/github", // Link to a page in your adm docs
+                label: "GitHub",
+            },
+            {
+                to: "/adm/category/coderunner", // Link to a page in your adm docs
+                label: "CodeRunner",
+            },
+            {
+                to: "/adm/category/stack", // Link to a page in your adm docs
+                label: "STACK",
+            },
+            {
+                to: "/adm/category/vpl", // Link to a page in your adm docs
+                label: "VPL",
+            },
+            {
+                to: "/adm/category/moosh-v2x", // Link to a page in your adm docs
+                label: "Moosh",
+            },
+        ],
     },
     {
         to: "/dev/intro", // Link to a page in your Dev docs
         label: "Developer",
+        type: "dropdown",
         position: "left",
         activeBaseRegex: `/dev/`, // Highlight when any Dev doc is active
+        items: [
+            {
+                to: "/dev/category/alpine-php-webserver", // Link to a page in your PJTS docs
+                label: "Alpine PHP Webserver",
+            },
+        ],
     },
     {
         to: "/edu/intro", // Link to a page in your Dev docs
         label: "Educator",
+        type: "dropdown",
         position: "left",
         activeBaseRegex: `/edu/`, // Highlight when any Dev doc is active
+        items: [
+            {
+                to: "/edu/intro", // Link to a page in your edu docs
+                label: "Intro",
+            },
+        ],
     },
     {
         to: "/qa/intro", // Link to a page in your QA docs
         label: "QA",
+        type: "dropdown",
         position: "left",
         activeBaseRegex: `/qa/`, // Highlight when any QA doc is active
+        items: [
+            {
+                to: "/qa/intro", // Link to a page in your QA docs
+                label: "Intro",
+            },
+        ],
     },
     { to: '/blog', label: 'Blog', position: 'left' },
     {
